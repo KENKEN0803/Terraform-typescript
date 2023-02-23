b271f73580229e6944c6de2234c91ca769332e12
KENKEN <kenken0803kr@gmail.com>
2023-02-23 09:14:01 +0900

    주석 추가

--- main.ts
+++ main.ts
@@ -1,33 +1,57 @@
-import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
-import { DockerProvider } from "@cdktf/provider-docker/lib/provider";
-import { Image } from "@cdktf/provider-docker/lib/image";
-import { Container } from "@cdktf/provider-docker/lib/container";
+import {Construct} from "constructs";
+import {App, TerraformStack} from "cdktf";
+import {DockerProvider} from "@cdktf/provider-docker/lib/provider";
+import {Image} from "@cdktf/provider-docker/lib/image";
+import {Container} from "@cdktf/provider-docker/lib/container";
 
+// CDKTF 스택을 생성합니다.
 class MyStack extends TerraformStack {
-  constructor(scope: Construct, name: string) {
-    super(scope, name);
+    constructor(scope: Construct, name: string) {
+        // TerraformStack을 생성합니다.
+        super(scope, name);
 
-    new DockerProvider(this, "docker", {});
+        // Docker Provider를 생성합니다.
+        new DockerProvider(this, "docker", {});
 
-    const dockerImage = new Image(this, "nginxImage", {
-      name: "nginx:latest",
-      keepLocally: false,
-    });
+        // Docker Image를 생성합니다.
+        const dockerImage = new Image(this, "nginxImage", {
+            name: "nginx:latest", // Docker Image 이름을 지정합니다.
+            keepLocally: false, // Docker Image를 로컬에 저장할지 여부를 지정합니다.
+        });
 
-    new Container(this, "nginxContainer", {
-      name: "tutorial",
-      image: dockerImage.name,
-      ports: [
-        {
-          internal: 80,
-          external: 8000,
-        },
-      ],
-    });
-  }
+        // Docker Container를 생성합니다.
+        new Container(this, "nginxContainer", {
+            name: "tutorial", // Docker Container 이름을 지정합니다.
+            image: dockerImage.name, // Docker Image 이름을 지정합니다.
+            ports: [ // Docker Container 포트를 지정합니다.
+                {
+                    internal: 80,
+                    external: 8000,
+                },
+            ],
+        });
+    }
 }
 
+// CDKTF 애플리케이션을 생성합니다.
 const app = new App();
+// CDKTF 스택을 생성합니다.
 new MyStack(app, "learn-cdktf-docker");
+// CDKTF 구성을 생성합니다.
 app.synth();
+
+/** 위의 CDKTF 코드는 다음 HCL 구성과 동일합니다.
+ * resource "docker_image" "nginx" {
+ *   name         = "nginx:latest"
+ *   keep_locally = false
+ * }
+ *
+ * resource "docker_container" "nginx" {
+ *   image = docker_image.nginx.name
+ *   name  = "tutorial"
+ *   ports {
+ *     internal = 80
+ *     external = 8000
+ *   }
+ * }
+ */
